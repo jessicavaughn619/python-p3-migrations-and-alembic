4c7dd8cf837325722562cc2a9049fbee9bdd3ea5
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-03-25 10:06:45 -0600

    Code along

---
+++ lib/migrations/versions/8eff515d29fe_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 8eff515d29fe
+Revises: 
+Create Date: 2023-03-25 10:00:24.080342
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8eff515d29fe'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
